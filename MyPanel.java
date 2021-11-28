d60f054b17050d6ea33835f61df06b8c5b89237c
yuxiRen <rita.ren.yuxi@gmail.com>
2021-11-27 19:09:09 -0800

    draw two tracks

--- MyPanel.java
+++ MyPanel.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
@@ -11,6 +12,25 @@ public class MyPanel extends JPanel{
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        g.fillRect(0, 0, 1000, 750  );
+        g.fillRect(0, 0, 1000, 750);
+        drawTank(hero.getX(), hero.getY(), g, 0, 0);
+    }
+
+    public void drawTank(int x, int y, Graphics g, int direction, int type) {
+        switch (type) {
+            case 0:
+                g.setColor(Color.cyan);
+                break;
+            case 1:
+                g.setColor(Color.yellow);
+                break;
+        }
+        switch (direction) {
+            case 0:
+                g.fill3DRect(x, y, 10, 60, false);
+                g.fill3DRect(x + 30, y, 10, 60, false);
+            default:
+                System.out.println("wrong direction");
+        }
     }
 }
