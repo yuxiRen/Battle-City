fcc5ed66c5c8b7e3774b4b63344e9700c8dde2b0
yuxiRen <rita.ren.yuxi@gmail.com>
2021-11-28 19:37:50 -0800

    add 4 directions

--- MyPanel.java
+++ MyPanel.java
@@ -3,7 +3,7 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-public class MyPanel extends JPanel {
+public class MyPanel extends JPanel implements KeyListener{
     MyTank hero = null;
 
     public MyPanel() {
@@ -13,7 +13,7 @@ public class MyPanel extends JPanel {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        g.fillRect(0, 0, 1000, 750);
+        g.fillRect(0, 0, 1000, 750);//draw a black panel
         drawTank(hero.getX(), hero.getY(), g, 0, 0);
     }
 
@@ -27,14 +27,34 @@ public class MyPanel extends JPanel {
                 break;
         }
         switch (direction) {
-            case 0:
+            case 0: //Up
                 g.fill3DRect(x, y, 10, 60, false);
                 g.fill3DRect(x + 30, y, 10, 60, false);
                 g.fill3DRect(x + 10, y + 10, 20, 40, false);
                 g.fillOval(x + 10, y + 20, 20, 20);
                 g.drawLine(x + 20, y + 30, x + 20, y);
-            default:
-                System.out.println("wrong direction");
+                break;
+            case 1: //Right
+                g.fill3DRect(x, y, 60, 10, false);
+                g.fill3DRect(x, y + 30, 60, 10, false);
+                g.fill3DRect(x + 10, y + 10, 40, 20, false);
+                g.fillOval(x + 20, y + 10, 20, 20);
+                g.drawLine(x + 30, y + 20, x + 60, y + 20);
+                break;
+            case 2: //Down
+                g.fill3DRect(x, y, 10, 60, false);
+                g.fill3DRect(x + 30, y, 10, 60, false);
+                g.fill3DRect(x + 10, y + 10, 20, 40, false);
+                g.fillOval(x + 10, y + 20, 20, 20);
+                g.drawLine(x + 20, y + 30, x + 20, y + 60);
+                break;
+            case 3: //Left
+                g.fill3DRect(x, y, 60, 10, false);
+                g.fill3DRect(x, y + 30, 60, 10, false);
+                g.fill3DRect(x + 10, y + 10, 40, 20, false);
+                g.fillOval(x + 20, y + 10, 20, 20);
+                g.drawLine(x + 30, y + 20, x, y + 20);
+                break;
         }
     }
 }
