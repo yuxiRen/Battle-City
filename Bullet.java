dae57908ce730e1b15b4f31805debc0cf1d93a29
yuxiRen <rita.ren.yuxi@gmail.com>
2021-11-30 20:25:10 -0800

    Add Bullet class

---
+++ Bullet.java
@@ -0,0 +1,43 @@
+public class Bullet implements Runnable {
+    int x;
+    int y;
+    int direction;
+    int speed = 2;
+    boolean isLive = true;
+
+    public Bullet(int x, int y, int direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            switch (direction) {
+                case 0:
+                    y -= speed;
+                    break;
+                case 1:
+                    x += speed;
+                    break;
+                case 2:
+                    y += speed;
+                    break;
+                case 3:
+                    x -= speed;
+                    break;
+            }
+            System.out.println(x + ", " + y);
+            if (x < 0 || x > 1000 || y < 0 || y > 750) {
+                isLive = false;
+                break;
+            }
+        }
+    }
+}
