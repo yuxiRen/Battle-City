1391c9c7c27a10f53c5deb5d068d389e83b7ee14
yuxiRen <rita.ren.yuxi@gmail.com>
2021-12-08 20:28:49 -0800

    let user decide if they want to continue last game

--- Game.java
+++ Game.java
@@ -1,16 +1,20 @@
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.Scanner;
 import javax.swing.JFrame;
 
 public class Game extends JFrame {
     MyPanel mp = null;
+    public static Scanner scr = new Scanner(System.in);
 
     public static void main(String[] args) {
         new Game();
     }
 
     public Game() {
-        mp = new MyPanel();
+        System.out.println("Do you want continue the last game? y or n");
+        String choice = scr.next();
+        mp = new MyPanel(choice);
         Thread thread = new Thread(mp);
         thread.start();
         this.add(mp);
